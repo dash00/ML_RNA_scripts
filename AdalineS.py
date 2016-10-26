265923a40c8a1dff324ff830b83d7ecb7675d5e0
Jeremy Coatelen <dash00@users.noreply.github.com>
2016-10-26 22:08:47 +0200

    Update AdalineS.py

--- AdalineS.py
+++ AdalineS.py
@@ -116,28 +116,3 @@ class AdalineS(object):
         """
         return np.where(self.activation(X) >= 0.0, 1, -1)
 
-if __name__ == '__main__':
-
-    import pandas as pd
-
-    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data', header=None)
-    y = df.iloc[0:100, 4].values
-    y = np.where(y == 'Iris-setosa', -1, 1)
-    X = df.iloc[0:100, [0, 2]].values
-    X_std = np.copy(X)
-    X_std[:,0] = (X[:,0] - X[:,0].mean()) / X[:,0].std()
-    X_std[:,1] = (X[:,1] - X[:,1].mean()) / X[:,1].std()
-
-    ada = AdalineS(iter_max=15, eta=0.01, random_state=1)
-    ada.fit(X_std, y)
-    plot_decision_regions(X_std, y, classifier=ada)
-    plt.title('Adaline - Stochastic Gradient Descent')
-    plt.xlabel('sepal length [standardized]')
-    plt.ylabel('petal length [standardized]')
-    plt.legend(loc='upper left')
-    plt.show()
-    plt.plot(range(1, len(ada.cost_) + 1), ada.cost_, marker='o')
-    plt.title('Adaline - SGD - Learning rate 0.01')
-    plt.xlabel('Epochs')
-    plt.ylabel('Average Cost')
-    plt.show()
